8f88afd51736bca946d56fd0b9f720f4845c6d09
Jalen Greene <terrell.greene95@gmail.com>
2025-03-21 01:11:12 -0500

    about ui

---
+++ src/app/(frontend)/about/page.tsx
@@ -0,0 +1,8 @@
+import About from "@/components/About";
+
+export const metadata = {
+  title: "About || Tokyo - Personal Portfolio React Nextjs Template",
+};
+export default function AboutPage() {
+  return <About />;
+}
--- src/app/(frontend)/services/page.tsx
+++ src/app/(frontend)/services/page.tsx
@@ -1,15 +1,5 @@
 import Services from "@/components/Services";
 
 export default function ServicesPages() {
-  return (
-    <div className="rightpart">
-      <div className="rightpart_in">
-        <div className="tokyo_tm_section">
-          <div data-aos="fade-right" data-aos-duration="1200">
-            <Services />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <Services />;
 }
