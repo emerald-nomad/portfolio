966754bbc3902bd2c246f73595e616585d076628
Jalen Greene <terrell.greene95@gmail.com>
2025-03-21 01:22:28 -0500

    contact ui

---
+++ src/app/(frontend)/contact/page.jsx
@@ -0,0 +1,13 @@
+import Contact from "@/components/Contact";
+export const metadata = {
+  title: "Contact || Tokyo - Personal Portfolio React Nextjs Template",
+};
+const index = () => {
+  return (
+    <>
+      <Contact />
+    </>
+  );
+};
+
+export default index;
