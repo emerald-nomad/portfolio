fb9686debb2b60ca705e9a8a9c609ba1528d7663
Jalen Greene <terrell.greene95@gmail.com>
2025-03-27 01:31:45 +0000

    react syntax highlighter

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,18 +1,15 @@
-'use client';
 
-import React, { useEffect } from "react";
-import Prism from "prismjs";
-import loadLanguages from 'prismjs/components/';
-loadLanguages(['c', 'rust', 'typescript', 'javascript', 'css', 'sass']);
+
+import React from "react";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark} from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+
 
 export function CodeBlock({ code, language }: {code: string; language: string}) {
-  useEffect(() => {
-    Prism.highlightAll();
-  }, []);
-
-  return (
-    <pre>
-      <code className={`language-${language}`}>{code}</code>
-    </pre>
+ return (
+    <SyntaxHighlighter language={language} style={atomDark}>
+      {code}
+    </SyntaxHighlighter>
   );
 }
